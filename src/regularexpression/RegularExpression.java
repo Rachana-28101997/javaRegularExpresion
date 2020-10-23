23b3a7c0d7b6152d21df569cab3c85058da4d0f3
Rachana <rachanarrachu28@gmail.com>
2020-10-23 22:09:00 +0530

    Added : Pattern to check user first name

--- src/regularexpression/RegularExpression.java
+++ src/regularexpression/RegularExpression.java
@@ -1,8 +1,19 @@
 package regularexpression;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Scanner;
 
 public class RegularExpression {
 	public static void main(String[] args){
-		System.out.println("Welcome to regular expression");
+		String regex = "^[A-Z]+[a-z]{2,}";
+		System.out.println("enter the pattern");
+		Scanner sc=new Scanner(System.in);
+		String text=sc.nextLine();
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(text);
+		boolean result=match.matches();
+		System.out.println(result);
+		sc.close();
 		
 	}
 }
