9c6337b334cd00de654864016d46ae518772412f
Rachana <rachanarrachu28@gmail.com>
2020-10-23 22:47:59 +0530

    Added : Pattern added to check the mail of user

--- src/regularexpression/RegularExpression.java
+++ src/regularexpression/RegularExpression.java
@@ -2,18 +2,18 @@ package regularexpression;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Scanner;
-
 public class RegularExpression {
-	public static void main(String[] args){
-		String regex1 = "^[A-Z]+[a-z]{2,}";
-		System.out.println("enter the last name");
-		Scanner sc1=new Scanner(System.in);
-		String text1=sc1.nextLine();
-		Pattern pattern1=Pattern.compile(regex1);
-		Matcher match1=pattern1.matcher(text1);
-		boolean result1=match1.matches();
-		System.out.println(result1);
-		sc1.close();
-		
+	public static void main(String[] args) {
+		String regex = "^[a-z]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$";
+		System.out.println("enter the pattern");
+		Scanner sc=new Scanner(System.in);
+		String text=sc.nextLine();
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(text);
+		boolean result=match.matches();
+		System.out.println(result);
+		sc.close();
 	}
+	
+
 }
